b8fb50a579611b03812fc7cf4a15ab4f4513406f
valentinFed <val.fediuk@gmail.com>
2019-04-09 21:43:27 +0200

    bugs with minuts and hours

--- main.js
+++ main.js
@@ -8,19 +8,43 @@ hours.innerText = date.getHours().toString().padStart(2, '0');
 minutes.innerText = date.getMinutes().toString().padStart(2, '0');
 seconds.innerText = date.getSeconds().toString().padStart(2, '0');
 
+// function* hour(start = 0, end = 12, step = 1) {
+//     for (let h = start; h <= end; h += step) {
+//         if (h < end) {
+//             yield h;
+//         } else {
+//             h = -1;
+//         }
+//     }
+// };
+
+// function* min(start = 0, end = 60, step = 1) {
+//     for (let m = start; m <= end; m += step) {
+//         if (m < end) {
+//             yield m;
+//         } else {
+//             m = -1;
+//             hours.innerText = genHour.next().value.toString().padStart(2, '0');
+//         }
+//     }
+// };
 
 function* sec(start = 0, end = 60, step = 1) {
     for (let s = start; s <= end; s += step) {
         if (s < end) {
-            yield s.toString().padStart(2, '0')
+            yield s.toString().padStart(2, '0');
         } else {
             s = -1;
+            // minutes.innerText = genMin.next().value.toString().padStart(2, '0');
+            // console.log(genMin.next().value);
         }
     }
 };
 
-let gen = sec(date.getSeconds());
+// let genHour = hour(date.getHours());
+// let genMin = min(date.getMinutes());
+let genSec = sec(date.getSeconds());
 
 setInterval(function () {
-    seconds.innerText = gen.next().value;
+    seconds.innerText = genSec.next().value;
 }, 1000);
