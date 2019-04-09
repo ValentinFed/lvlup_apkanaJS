a4149d571b9edfaf33b5f73e36ab9f301b862d96
valentinFed <val.fediuk@gmail.com>
2019-04-09 19:08:06 +0200

    seconds done

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 let hours = document.getElementById("main__hours");
-let minuts = document.getElementById("main__minuts");
+let minutes = document.getElementById("main__minuts");
 let seconds = document.getElementById("main__seconds");
 
 let date = new Date();
 
 hours.innerText = date.getHours().toString().padStart(2, '0');
-minuts.innerText = date.getMinutes().toString().padStart(2, '0');
+minutes.innerText = date.getMinutes().toString().padStart(2, '0');
 seconds.innerText = date.getSeconds().toString().padStart(2, '0');
+
+
+function* sec(start = 0, end = 60, step = 1) {
+    for (let s = start; s <= end; s += step) {
+        if (s < end) {
+            yield s.toString().padStart(2, '0')
+        } else {
+            s = -1;
+        }
+    }
+};
+
+let gen = sec(date.getSeconds());
+
+setInterval(function () {
+    seconds.innerText = gen.next().value;
+}, 1000);
